9a80bcbf18a41f0f6a88a04460adc5fe0825f46d
joieambrose-lab <joie.ambrose@telegraph.co.uk>
2026-05-15 13:21:02 +0100

    Challenge 3: Dynamic Routing for Articles (#2)
    * Challenge 3: Dynamic Routing for Articles
    
    * interface fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,14 @@
 import articles from '@/data/articles.json'
 import Link from 'next/link'
 
-interface Article {
-  id: number
-  title: string
-  slug: string
-  author: string
-  date: string
-  content: string
-  image: string
-  category: string
-}
+type Article = (typeof articles)[number];
 
 export default async function Home() {
   return (
     <main>
       {(articles as Article[]).map((article) => (
   <div key={article.id}>
-    <Link href={`/${article.slug}`}>{article.title}</Link>
+    <Link href={`/articles/${article.slug}`}>{article.title}</Link>
     <p>{article.date}</p>
   </div>
 ))}
---
+++ src/types/article.ts
@@ -0,0 +1,12 @@
+interface Article {
+  id: number
+  title: string
+  slug: string
+  author: string
+  date: string
+  content: string
+  image: string
+  category: string
+}
+
+export default Article;
